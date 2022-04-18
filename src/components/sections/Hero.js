fa982fd449d175e207ed91b421187e8475562266
Jack-R-Long <jack.richard.long@gmail.com>
2022-04-18 18:20:36 +0000

    Start adding custom content

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -61,18 +61,18 @@ const Hero = ({
         <div className={innerClasses}>
           <div className="hero-content">
             <h1 className="mt-0 mb-16 reveal-from-bottom" data-reveal-delay="200">
-              Landing template for <span className="text-color-primary">startups</span>
+              Fly <span className="text-color-primary">Apes</span>
             </h1>
             <div className="container-xs">
               <p className="m-0 mb-32 reveal-from-bottom" data-reveal-delay="400">
-                Our landing page template works on all devices, so you only have to set it up once, and get beautiful results forever.
+                NFT Project with flying apes!  
                 </p>
               <div className="reveal-from-bottom" data-reveal-delay="600">
                 <ButtonGroup>
-                  <Button tag="a" color="primary" wideMobile href="https://cruip.com/">
+                  <Button tag="a" color="primary" wideMobile href="https://github.com/Jack-R-Long">
                     Get started
                     </Button>
-                  <Button tag="a" color="dark" wideMobile href="https://github.com/cruip/open-react-template/">
+                  <Button tag="a" color="dark" wideMobile href="https://github.com/Jack-R-Long">
                     View on Github
                     </Button>
                 </ButtonGroup>
@@ -80,7 +80,7 @@ const Hero = ({
             </div>
           </div>
           <div className="hero-figure reveal-from-bottom illustration-element-01" data-reveal-value="20px" data-reveal-delay="800">
-            <a
+            {/* <a
               data-video="https://player.vimeo.com/video/174002812"
               href="#0"
               aria-controls="video-modal"
@@ -92,7 +92,14 @@ const Hero = ({
                 alt="Hero"
                 width={896}
                 height={504} />
-            </a>
+            </a> */
+              <Image
+              className="has-shadow"
+              src={require('./../../assets/images/f22.jpg')}
+              alt="Hero"
+              width={896}
+              height={504} />
+              }
           </div>
           <Modal
             id="video-modal"
